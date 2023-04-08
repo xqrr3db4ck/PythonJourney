902c5a3ceac1c498d87362420fc43af31593efb1
xqrr3db4ck <121759012+xqrr3db4ck@users.noreply.github.com>
2023-04-07 19:03:30 -0500

    Add files via upload

---
+++ 1To10WhileLoop.py
@@ -0,0 +1,5 @@
+spam = 0
+while spam <= 10:
+    print(str(spam))
+    spam = spam + 1
+
---
+++ exerciseNo9.py
@@ -0,0 +1,11 @@
+spam = ''
+
+print('Hello... I think in a number between 1 or 2, guess the number')
+spam = str(input())
+
+if spam == '1':
+    print('Hello')
+elif spam == '2':
+    print('Howdy')
+else:
+    print('Greetings, you make your own rules!')
