4a3589ee17bd39b6382452c378a51f830b0c6724
xqrr3db4ck <121759012+xqrr3db4ck@users.noreply.github.com>
2023-04-08 11:33:56 -0500

    Update keylogger.py

--- WindowsAutomation/keylogger.py
+++ WindowsAutomation/keylogger.py
@@ -4,14 +4,16 @@ import shutil
 
 df = pd.read_excel('pedidos.xlsx')
 df.drop(df.columns[[1, 2, 3, 4, 8, 17, 18, 19]], axis=1, inplace=True)
-df.iloc[:,0] = df.iloc[:,0].str[5:]
-df.iloc[:,1] = df.iloc[:,1].str[6:]
-df.iloc[:,3] = df.iloc[:,3].str[6:]
+df.iloc[:, 0] = df.iloc[:, 0].str[5:]
+df.iloc[:, 1] = df.iloc[:, 1].str[6:]
+df.iloc[:, 3] = df.iloc[:, 3].str[6:]
 df['COD_PUB'] = df['COD_PUB'].str.lstrip('0') + '_'
 df['TITULO'] = df['TITULO'].str.slice(stop=30)
+df['TITULO'] = df['TITULO'].astype(str) + '  -'
 df['EDIT'] = df['EDIT'].str.slice(stop=-4)
-df['CONT'] = df['CONT'].replace({'BNOBR090': 'B90', 'BNOBR080': 'HOL.'})
-df['TAPA'] = df['TAPA'].replace({'TAILU270': 'ESM300'})
-df['ENC'] = df['ENC'].replace({'ENCBIN': 'RÚST.'})
-df['LAM'] = df['LAM'].replace({'LAMMAT': 'MATE', 'LAMBTE': 'BTE'})
+df.replace({'BNAHU080': 'HOL.', 'BNOBR080': 'B70', 'COOBR090': 'B90', 'BNOBR090': 'B90', 'BNILM115': 'E115',
+            'COAHU080': 'HOL.', 'TAILU270': 'ESM300', 'ENCBIN': 'RÚST.', 'ENCACA': 'CABA.', 'LAMMAT': 'MAT',
+            'LAMBTE': 'BTE'}, regex=True, inplace=True)
+df['EDIT'] = df.pop('EDIT')
+df.insert(0, 'NO.', range(1, len(df) + 1))
 df.to_excel('pedidos.xlsx', index=False)
