2454959e13dd502e4f25e1bb0205b1b079d55989
xqrr3db4ck <121759012+xqrr3db4ck@users.noreply.github.com>
2023-04-09 18:52:52 -0500

    Update keylogger.py

--- WindowsAutomation/keylogger.py
+++ WindowsAutomation/keylogger.py
@@ -16,8 +16,21 @@ df.replace({'BNAHU080': 'HOL.', 'BNOBR080': 'B70', 'COOBR090': 'B90', 'BNOBR090'
             'LAMBTE': 'BTE'}, regex=True, inplace=True)
 df['EDIT'] = df.pop('EDIT')
 df.insert(0, 'NO.', range(1, len(df) + 1))
-df.to_excel('PedidosBMG1a1.xlsx', index=False)
-df = pd.read_excel('PedidosBMG1a1.xlsx')
-df.to_csv('BuscadorDePedidos.csv', index=False)
-df = pd.read_csv('BuscadorDePedidos.csv', usecols=[2, 12])
-print(df)
+df_ordenado = df.copy()
+df_ordenado.iloc[:, 1:] = df_ordenado.iloc[:, 1:].sort_values(by=['EDIT'], ascending=[True])
+df_ordenado.to_excel('PedidosBMG1a1_sorted.xlsx', index=False)
+dir12 = pd.read_excel('PedidosBMG1a1_sorted.xlsx', usecols=[12])
+fil2 = pd.read_excel('PedidosBMG1a1_sorted.xlsx', usecols=[2])
+print(dir12)
+print(fil2)
+root_dir = '/home/crxxp919/PycharmProjects'
+subdirs = [x[0] for x in os.walk(root_dir)]
+dest_dir = '/home/crxxp919/PycharmProjects/pythonProject'
+
+for subdir in subdirs:
+    for val in dir12.values:
+        if str(val[0]) in subdir:
+            for filename in os.listdir(subdir):
+                for fileval in fil2.values:
+                    if str(fileval[0]) in filename and filename.endswith('.pdf'):
+                        shutil.copy(os.path.join(subdir, filename), dest_dir)
