daa4664a6a46bdb662e523ab367b3c2d6f81a5d7
xqrr3db4ck <121759012+xqrr3db4ck@users.noreply.github.com>
2023-04-09 19:19:55 -0500

    Update keylogger.py

--- WindowsAutomation/keylogger.py
+++ WindowsAutomation/keylogger.py
@@ -1,16 +1,12 @@
-import pandas as pd
-import os
-import shutil
-
 df = pd.read_excel('pedidos.xlsx')
 df.drop(df.columns[[1, 2, 3, 4, 8, 17, 18, 19]], axis=1, inplace=True)
 df.iloc[:, 0] = df.iloc[:, 0].str[5:]
-df.iloc[:, 1] = df.iloc[:, 1].str[6:]
+df.iloc[:, 1] = df.iloc[:, 1].str[5:]
 df.iloc[:, 3] = df.iloc[:, 3].str[6:]
 df['COD_PUB'] = df['COD_PUB'].str.lstrip('0') + '_'
 df['TITULO'] = df['TITULO'].str.slice(stop=30)
 df['TITULO'] = df['TITULO'].astype(str) + '  -'
-df['EDIT'] = df['EDIT'].str.slice(stop=-4)
+df['EDIT'] = df['EDIT'].str.slice(stop=-3)
 df.replace({'BNAHU080': 'HOL.', 'BNOBR080': 'B70', 'COOBR090': 'B90', 'BNOBR090': 'B90', 'BNILM115': 'E115',
             'COAHU080': 'HOL.', 'TAILU270': 'ESM300', 'ENCBIN': 'RÚST.', 'ENCACA': 'CABA.', 'LAMMAT': 'MAT',
             'LAMBTE': 'BTE'}, regex=True, inplace=True)
@@ -18,9 +14,9 @@ df['EDIT'] = df.pop('EDIT')
 df.insert(0, 'NO.', range(1, len(df) + 1))
 df_ordenado = df.copy()
 df_ordenado.iloc[:, 1:] = df_ordenado.iloc[:, 1:].sort_values(by=['EDIT'], ascending=[True])
-df_ordenado.to_excel('PedidosBMG1a1_sorted.xlsx', index=False)
-dir12 = pd.read_excel('PedidosBMG1a1_sorted.xlsx', usecols=[12])
-fil2 = pd.read_excel('PedidosBMG1a1_sorted.xlsx', usecols=[2])
+df_ordenado.to_excel('PedidosBMG1a1.xlsx', index=False)
+dir12 = pd.read_excel('PedidosBMG1a1.xlsx', usecols=[12])
+fil2 = pd.read_excel('PedidosBMG1a1.xlsx', usecols=[2])
 print(dir12)
 print(fil2)
 root_dir = '/home/crxxp919/PycharmProjects'
