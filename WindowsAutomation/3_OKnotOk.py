e3292262b16c76e8b5f2fde0f805eaee81973a10
xqrr3db4ck <121759012+xqrr3db4ck@users.noreply.github.com>
2023-04-28 11:21:49 -0500

    Update 3_OKnotOk.py

--- WindowsAutomation/3_OKnotOk.py
+++ WindowsAutomation/3_OKnotOk.py
@@ -83,21 +83,8 @@ wb.save('_1a1BMG_.xlsx')
 ###
 ###
 df = pd.read_excel('_1a1BMG_.xlsx')
-df['ANCHOALTO'] = df['ANCHOALTO'].str.replace('x', '').astype(int)
-df['result'] = df.apply(lambda row: math.ceil(int(row['CANT']) * int(row['PAG']) / 16) if int(row['ANCHOALTO']) <= 170240 else math.ceil(int(row['CANT']) * int(row['PAG']) / 32), axis=1)
-results = {'B70': [], 'B90': [], 'HOL.': []}
-for index, row in df.iterrows():
-    if row['CONT'] == 'B70':
-        results['B70'].append(row['CANT'] * row['PAG'] / 32 if row['ANCHOALTO'] < 170240 else row['CANT'] * row['PAG'] / 16)
-    elif row['CONT'] == 'B90':
-        results['B90'].append(row['CANT'] * row['PAG'] / 32 if row['ANCHOALTO'] < 170240 else row['CANT'] * row['PAG'] / 16)
-    elif row['CONT'] == 'HOL.':
-        results['HOL.'].append(row['CANT'] * row['PAG'] / 32 if row['ANCHOALTO'] < 170240 else row['CANT'] * row['PAG'] / 16)
-b70 = sum(results['B70'])
-b90 = sum(results['B90'])
-hol = sum(results['HOL.'])
-df.loc[df.index[-1] + 1] = ['' for _ in range(len(df.columns))]
-df.iloc[-1, 1] = math.ceil(sum(results['B70']))
-df.iloc[-1, 2] = math.ceil(sum(results['B90']))
-df.iloc[-1, 3] = math.ceil(sum(results['HOL.']))
+df['result'] = df.apply(lambda row: row['CANT'] * row['PAG'] / 32 if row['ANCHOALTO'].replace('x', '') <= '170240' else row['CANT'] * row['PAG'] / 16, axis=1)
+B70 = df.loc[df['CONT'] == 'B70', 'result']
+B90 = df.loc[df['CONT'] == 'B90', 'result']
+HOL = df.loc[df['CONT'] == 'HOL.', 'result']
 df.to_excel('_1a1BMG_.xlsx', index=False)
