d6d4a736cb92b36703c9f834418d024fe13df044
xqrr3db4ck <121759012+xqrr3db4ck@users.noreply.github.com>
2023-07-07 15:43:11 -0500

    Update 1a1.py

--- WindowsAutomation/1a1.py
+++ WindowsAutomation/1a1.py
@@ -5,21 +5,24 @@ from math import ceil
 import os
 
 df = pd.read_excel('1a1.xlsx')
-df.drop(df.columns[[1, 2, 3, 4, 8, 17, 18, 19]], axis=1, inplace=True)
+df.drop(df.columns[[2, 3, 4, 8, 17, 18, 19]], axis=1, inplace=True)
+df['FECHA_PED'] = df.pop('FECHA_PED')
 df.iloc[:, 0] = df.iloc[:, 0].str[5:]
 df.iloc[:, 1] = df.iloc[:, 1].str[5:]
 df.iloc[:, 3] = df.iloc[:, 3].str[6:]
 df['COD_PUB'] = df['COD_PUB'].str.lstrip('0') + '_'
-df['TITULO'] = df['TITULO'].str.slice(stop=30)
+df['TITULO'] = df['TITULO'].str.slice(stop=28)
 df['TITULO'] = df['TITULO'].astype(str) + '  -'
 df['EDIT'] = df['EDIT'].str.slice(stop=-3)
 df['EDIT'] = df['EDIT'].str.lstrip('0')
-df.replace({'BNAHU080': 'HOL', 'BNOBR080': 'B70', 'COOBR090': 'B90', 'BNOBR090': 'B90', 'BNILM115': 'E115',
-            'COAHU080': 'HOL', 'TAILU270': 'ESM300', 'ENCBIN': 'RÚST.', 'ENCACA': 'CABA.', 'LAMMAT': 'MAT',
-            'LAMBTE': 'BTE', 'COILM090': 'MAT90'}, regex=True, inplace=True)
 df['EDIT'] = df.pop('EDIT')
 df['PWSH'] = df['COD_PUB']
 df['PWSH'] = df['PWSH'].str.lstrip('0') + '*,'
+df['FECHA_PED'] = df['FECHA_PED'].str.slice(stop=2) ###N
+df['FECHA_PED'] = df.pop('FECHA_PED')
+df.replace({'BNAHU080': 'HOL', 'BNOBR080': 'B70', 'COOBR090': 'B90', 'BNOBR090': 'B90', 'BNILM115': 'E115',
+            'COAHU080': 'HOL', 'TAILU270': 'ESM300', 'ENCBIN': 'RÚST.', 'ENCACA': 'CABA.', 'LAMMAT': 'MAT',
+            'LAMBTE': 'BTE', 'COILM090': 'MAT90'}, regex=True, inplace=True)
 new_file_name = "_1a1_.xlsx"
 df.to_excel(new_file_name, index=False)
 print(f"File saved as {new_file_name}")
